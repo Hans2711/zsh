6a9c947b7265c71a875a0baec7775ba3788dbd5d
Hans Peter Diesing <hans.diesing@netigo.de>
2025-03-20 13:02:02 +0100

    Add gitignore, gitmodules, aliases, env, git, keychain, platform<commit message>

---
+++ aliases.sh
@@ -0,0 +1,6 @@
+alias l='ls -lahF'
+ 
+# Docker
+alias docker-compose='docker compose'
+
+source ~/.config/zsh/git.sh
---
+++ zshrc.sh
@@ -0,0 +1,21 @@
+export ZSH="$HOME/.oh-my-zsh"
+ZSH_THEME="amuse"
+HYPHEN_INSENSITIVE="true"
+zstyle ':omz:update' mode auto
+# Uncomment the following line to enable command auto-correction.
+ENABLE_CORRECTION="true"
+plugins=(copyfile copypath sudo dirhistory docker laravel npm)
+source $ZSH/oh-my-zsh.sh
+export MANPATH="/usr/local/man:$MANPATH"
+export LANG=en_US.UTF-8
+
+if [[ -n $SSH_CONNECTION ]]; then
+  export EDITOR='vim'
+else
+  export EDITOR='nvim'
+fi
+
+source ~/.config/zsh/aliases.sh
+source ~/.config/zsh/env.sh
+source ~/.config/zsh/platform.sh
+source ~/.config/zsh/zsh-autocomplete/zsh-autocomplete.plugin.zsh
